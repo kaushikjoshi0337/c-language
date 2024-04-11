87ca1af84a588c02b57ed9f5b952d5439ad89cb2
dharmi9397 <patelzerox2008@gemail.com>
2024-04-11 12:20:07 +0530

    clanguage

---
+++ JOSHI1.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a = 12;
+int b = 6;
+
+clrscr();
+ printf("addition:%d\n",a+b);
+ printf("subtraction:%d\n",a-b);
+ printf("multiplication:%d\n",a*b);
+ printf("division:%d\n", a/b);
+ printf("modular:%d\n",a%b);
+
+
+
+getch();
+}
